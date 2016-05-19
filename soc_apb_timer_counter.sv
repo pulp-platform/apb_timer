bdfa6daacb9f4b59aa34d4c120efec839d371a39
IGOR LOI <igor.loi@unibo.it>
2016-05-19 10:55:10 +0200

    fixed target_reached_o, was targetted as blocking and non blocking, with errors in synth

--- soc_apb_timer_counter.sv
+++ soc_apb_timer_counter.sv
@@ -9,55 +9,55 @@
 // specific language governing permissions and limitations under the License.
 
 module soc_apb_timer_counter
-  (
+(
    input  logic        clk_i,
    input  logic        rst_ni,
-   
+
    input  logic        reset_count_i,
    input  logic        enable_count_i,
    input  logic [31:0] compare_value_i,
-   
+
    output logic [31:0] counter_value_o,
    output logic        target_reached_o
-   );
-   
+);
+
    logic [31:0]        s_count, s_count_reg;
-   
+
    // COUNTER
    always_comb
-     begin
-	s_count = s_count_reg;
-	
-	// start counting
-	if ( reset_count_i == 1 )
-	  s_count = 0;
-	else
-	  begin
-	     if ( enable_count_i == 1 ) // the counter is increased if counter is enabled and there is a tick
-	       s_count = s_count_reg + 1;
-	  end
-     end
-   
+   begin
+      s_count = s_count_reg;
+
+      // start counting
+      if ( reset_count_i == 1 )
+         s_count = 0;
+      else
+      begin
+         if ( enable_count_i == 1 ) // the counter is increased if counter is enabled and there is a tick
+            s_count = s_count_reg + 1;
+      end
+   end
+
    always_ff@(posedge clk_i, negedge rst_ni)
-     begin
-	if (rst_ni == 0)
-	  s_count_reg <= 0;
-	else
-	  s_count_reg <= s_count;
-     end
-   
+   begin
+      if (rst_ni == 0)
+         s_count_reg <= 0;
+      else
+         s_count_reg <= s_count;
+   end
+
    // COMPARATOR
    always_ff@(posedge clk_i, negedge rst_ni)
-     begin
-	if (rst_ni == 0)
-	   target_reached_o <= 1'b0;
-	else
-	  if ( s_count == compare_value_i )
-	    target_reached_o = 1'b1;
-	  else
-	    target_reached_o = 1'b0;
-     end
+   begin
+      if (rst_ni == 0)
+         target_reached_o <= 1'b0;
+      else
+         if ( s_count == compare_value_i )
+            target_reached_o <= 1'b1;
+         else
+            target_reached_o <= 1'b0;
+   end
    
    assign counter_value_o = s_count_reg;
-   
+
 endmodule
